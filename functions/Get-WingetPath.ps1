f641cb160c6e9b2179ae6049ce051a4ba6856183
unknown <dave.just@gmail.com>
2022-08-12 18:58:09 -0400

    Replaced hard coded Program Files path with Environment Variable

--- functions/Get-WingetPath.ps1
+++ functions/Get-WingetPath.ps1
@@ -3,7 +3,7 @@ $currentUser = whoami # get account running function
 	if ($currentUser -eq "nt authority\system")
 	{
 	# check if running in SYSTEM context set path to WINGET executable
-	$winget = Get-ChildItem -path "C:\Program Files\WindowsApps" -Recurse -File | where-object { $_.name -like "AppInstallerCLI.exe" -or $_.name -like "WinGet.exe" } | Select-Object -ExpandProperty fullname
+	$winget = Get-ChildItem -path "$env:ProgramFiles\WindowsApps" -Recurse -File | where-object { $_.name -like "AppInstallerCLI.exe" -or $_.name -like "WinGet.exe" } | Select-Object -ExpandProperty fullname
 	# If there are multiple versions, select latest
 	if ($winget.count -gt 1) { $winget = $winget[-1] }
 	}
